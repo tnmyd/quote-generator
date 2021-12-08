ef12ebc514f1eaa2a14163548850ba2c2135cb02
Tanmoy Das <tanmoydas1997@gmail.com>
2021-12-08 17:13:01 +0530

    Add Copy to Clipboard functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const quoteElement = document.querySelector(".quote")
 const authorElement = document.querySelector(".author")
 const newQuoteButton = document.querySelector(".new-quote")
+const copyButton = document.querySelector(".copy-clipboard")
 
 //Get quotes from API if it is not present in local storage
 let apiQuotes = JSON.parse(localStorage.getItem("quotes")) || null
@@ -19,6 +20,15 @@ function showQuote() {
 	let randomIndex = Math.floor(Math.random() * apiQuotes.length)
 	quoteElement.innerText = apiQuotes[randomIndex].text
 	authorElement.innerText = "—" + apiQuotes[randomIndex].author
+	copyButton.innerText = "Copy to Clipboard"
+	copyButton.disabled = false
 }
 window.addEventListener("load", showQuote)
 newQuoteButton.addEventListener("click", showQuote)
+
+copyButton.addEventListener("click", async () => {
+	let text = quoteElement.innerText + authorElement.innerText
+	await navigator.clipboard.writeText(text)
+	copyButton.innerText = "Copied"
+	copyButton.disabled = true
+})

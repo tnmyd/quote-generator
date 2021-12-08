df19529a0c631f39f5a67472c12dac069718c721
Tanmoy Das <tanmoydas1997@gmail.com>
2021-12-08 13:31:41 +0530

    Cache quotes in local storage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const quoteElement = document.querySelector(".quote")
 const authorElement = document.querySelector(".author")
 const newQuoteButton = document.querySelector(".new-quote")
-let apiQuotes = null
+let apiQuotes = JSON.parse(localStorage.getItem("quotes")) || null
 if (apiQuotes == null) {
 	getQuotes()
 }
@@ -9,6 +9,7 @@ async function getQuotes() {
 	const apiUrl = "https://type.fit/api/quotes"
 	const response = await fetch(apiUrl)
 	apiQuotes = await response.json()
+	localStorage.setItem("quotes", JSON.stringify(apiQuotes))
 }
 newQuoteButton.addEventListener("click", () => {
 	console.log(apiQuotes)

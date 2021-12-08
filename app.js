b92652ef3ab442d5f66d9df121a1e8a7a8fb6a43
Tanmoy Das <tanmoydas1997@gmail.com>
2021-12-08 15:42:19 +0530

    Display random quote on load

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const quoteElement = document.querySelector(".quote")
 const authorElement = document.querySelector(".author")
 const newQuoteButton = document.querySelector(".new-quote")
+
+//Get quotes from API if it is not present in local storage
 let apiQuotes = JSON.parse(localStorage.getItem("quotes")) || null
 if (apiQuotes == null) {
 	getQuotes()
 }
+
 async function getQuotes() {
 	const apiUrl = "https://type.fit/api/quotes"
 	const response = await fetch(apiUrl)
 	apiQuotes = await response.json()
 	localStorage.setItem("quotes", JSON.stringify(apiQuotes))
 }
-newQuoteButton.addEventListener("click", () => {
-	console.log(apiQuotes)
-	quoteElement.innerText = apiQuotes[0].text
-	authorElement.innerText = "—" + apiQuotes[0].author
-})
+
+function showQuote() {
+	let randomIndex = Math.floor(Math.random() * apiQuotes.length)
+	quoteElement.innerText = apiQuotes[randomIndex].text
+	authorElement.innerText = "—" + apiQuotes[randomIndex].author
+}
+window.addEventListener("load", showQuote)
+newQuoteButton.addEventListener("click", showQuote)

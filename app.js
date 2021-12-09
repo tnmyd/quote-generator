df2f54f5995a5be5da483f263e219aa958d983c1
Tanmoy Das <tanmoydas1997@gmail.com>
2021-12-09 22:47:57 +0530

    Add loader to wait for fetch

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const authorElement = document.querySelector(".author")
 const newQuoteButton = document.querySelector(".new-quote")
 const copyButton = document.querySelector(".copy-clipboard")
 const tweetButton = document.querySelector(".twitter")
+const loader = document.querySelector(".loader")
+const quoteContainer = document.querySelector("main")
 
 //Get quotes from API if it is not present in local storage
 let apiQuotes = JSON.parse(localStorage.getItem("quotes")) || null
 if (apiQuotes == null) {
 	getQuotes()
+} else {
+	showQuote()
+}
+function showLoader() {
+	loader.hidden = false
+	quoteContainer.hidden = true
+}
+function hideLoader() {
+	loader.hidden = true
+	quoteContainer.hidden = false
 }
 
 async function getQuotes() {
+	showLoader()
 	const apiUrl = "https://type.fit/api/quotes"
 	const response = await fetch(apiUrl)
 	apiQuotes = await response.json()
 	localStorage.setItem("quotes", JSON.stringify(apiQuotes))
+	showQuote()
 }
 
 function showQuote() {
@@ -22,6 +36,7 @@ function showQuote() {
 	quoteElement.innerText = apiQuotes[randomIndex].text
 	const author = apiQuotes[randomIndex].author || "Anonymous"
 	authorElement.innerText = "—" + author
+	hideLoader()
 	copyButton.innerText = "Copy to Clipboard"
 	copyButton.disabled = false
 }
